cd91df3b23583225f130f17d9f5b2862b01f4cb2
VolatileX1 <128452078+VolatileX1@users.noreply.github.com>
2023-06-02 12:50:29 +0530

    Customizable Options
    - Added functionality to customize password generation process using optional arguments and user prompts. 
    - Users can now specify length, inclusion of digits and punctuation, and more.

--- passforge.py
+++ passforge.py
@@ -1,13 +1,41 @@
 import random
 import string
 
-def generate_password(length):
-    """Generate a random password of specified length"""
-    characters = string.ascii_letters + string.digits + string.punctuation
+def generate_password(length=12, include_digits=True, include_punctuation=True):
+    """Generate a random password with customizable options"""
+    characters = string.ascii_letters
+    if include_digits:
+        characters += string.digits
+    if include_punctuation:
+        characters += string.punctuation
     password = ''.join(random.choice(characters) for i in range(length))
-    return password, 'weak' if len(password) < 8 else 'medium' if len(password) < 12 else 'strong'
+    return password
+
+def check_password_strength(password):
+    """Check the strength of a password and rate it as weak, medium, or strong"""
+    rating = 0
+    if len(password) >= 8:
+        rating += 1
+    if any(c.isupper() for c in password) and any(c.islower() for c in password):
+        rating += 1
+    if any(c.isdigit() for c in password):
+        rating += 1
+    if any(c in string.punctuation for c in password):
+        rating += 1
+    if len(password) >= 12:
+        rating += 1
+    if rating == 0:
+        return 'weak'
+    elif rating < 4:
+        return 'medium'
+    else:
+        return 'strong'
 
 if __name__ == '__main__':
-    length = int(input("Enter password length: "))
-    password, strength = generate_password(length)
-    print(f"Your {strength} password is:", password)
+    length = int(input("Enter desired password length: "))
+    include_digits = input("Include digits? (y/n): ").lower() == 'y'
+    include_punctuation = input("Include punctuation? (y/n): ").lower() == 'y'
+    password = generate_password(length, include_digits, include_punctuation)
+    print(f"Your password is: {password}")
+    strength = check_password_strength(password)
+    print(f"The strength of your password is {strength}.")
